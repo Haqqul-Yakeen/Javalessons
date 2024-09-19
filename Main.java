f91f7b17b011ab3f9a1a03968d5e79199a78ce4e
Haqqul-Yakeen <haqqulyakeen8@gmail.com>
2024-09-19 13:47:49 +0000

    a simple home page in html

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++)
+            System.out.println(52);
+    }
+}
